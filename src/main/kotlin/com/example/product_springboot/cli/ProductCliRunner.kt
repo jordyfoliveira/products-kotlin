5547e5a5c4311b5c43245c92943f492b41a2f0eb
jordyfoliveira <jordyfdeoliveira_particular@hotmail.com>
2026-04-29 00:56:37 +0100

    update cli class

--- src/main/kotlin/com/example/product_springboot/cli/ProductCliRunner.kt
+++ src/main/kotlin/com/example/product_springboot/cli/ProductCliRunner.kt
@@ -1,4 +1,33 @@
 package com.example.product_springboot.cli
 
-class ProductCliRunner {
+import com.example.product_springboot.service.ProductService
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Component
+import java.util.Scanner
+
+@Component
+class ProductCliRunner (private val productService: ProductService) : CommandLineRunner {
+
+    override fun run(vararg args: String) {
+        menu()
+    }
+
+    fun menu(){
+        val scanner = Scanner(System.`in`)
+
+        while(true) {
+            println("1) Listar produtos")
+            println("2) Ver produto por ID")
+            println("0) Sair")
+
+            val option = scanner.nextLine()
+
+            when(option){
+                "1" -> println("Listar produtos")
+                "2" -> println("Ver produto por ID")
+                "0" -> return
+                else -> println("Opção inválida")
+            }
+        }
+    }
 }
