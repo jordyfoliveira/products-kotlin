bdfba949a6310c3d1bd8382a93f384a1b657b8a1
jordyfoliveira <jordyfdeoliveira_particular@hotmail.com>
2026-05-11 16:19:56 +0100

    Add CLI

--- src/main/kotlin/com/example/product_springboot/cli/ProductCliRunner.kt
+++ src/main/kotlin/com/example/product_springboot/cli/ProductCliRunner.kt
@@ -4,6 +4,7 @@ import com.example.product_springboot.service.ProductService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
 import java.util.Scanner
+import org.springframework.web.server.ResponseStatusException
 
 @Component
 class ProductCliRunner (private val productService: ProductService) : CommandLineRunner {
@@ -23,8 +24,38 @@ class ProductCliRunner (private val productService: ProductService) : CommandLin
             val option = scanner.nextLine()
 
             when(option){
-                "1" -> println("Listar produtos")
-                "2" -> println("Ver produto por ID")
+                "1" -> {
+                    val products = productService.getAllProducts()
+
+                    if (products.isEmpty()) {
+                        println("Nenhum produto encontrado")
+                    } else {
+                        products.forEach {
+                            println("ID: ${it.id} | SKU: ${it.sku} | Nome: ${it.name} | Preço: ${it.price} | Stock: ${it.stock}")
+                        }
+                    }
+                }
+                "2" -> {
+                    print("Introduza o ID do produto: ")
+                    val idInput = scanner.nextLine()
+
+                    val id = idInput.toLongOrNull()
+
+                    if (id == null) {
+                        println("ID inválido.")
+                    } else {
+                        try {
+                            val product = productService.getProductById(id)
+
+                            println(
+                                "ID: ${product.id} | SKU: ${product.sku} | Nome: ${product.name} | " +
+                                        "Descrição: ${product.description} | Preço: ${product.price} | Stock: ${product.stock}"
+                            )
+                        } catch (ex: ResponseStatusException) {1
+                            println(ex.reason ?: "Produto não encontrado")
+                        }
+                    }
+                }
                 "0" -> return
                 else -> println("Opção inválida")
             }
